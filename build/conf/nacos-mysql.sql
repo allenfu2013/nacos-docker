9c1e23c513b059ff3e79059ba2ccd17dabb6cb7d
Jinghua Feng <jinghua.feng@kyligence.io>
2020-09-02 10:13:47 +0800

    add item table

--- build/conf/nacos-mysql.sql
+++ build/conf/nacos-mysql.sql
@@ -39,6 +39,33 @@ CREATE TABLE `config_info` (
   UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='config_info';
 
+
+CREATE TABLE `config_info_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `data_id` varchar(255) NOT NULL COMMENT 'data_id',
+  `group_id` varchar(255) DEFAULT NULL,
+  `item_key` varchar(128) NOT NULL,
+	`item_value` longtext NOT NULL COMMENT 'content',
+  `item_comment` varchar(128) DEFAULT NULL ,
+  `item_meaning` varchar(128) DEFAULT NULL ,
+  `restart_effect` boolean NOT NULL DEFAULT false,
+  `item_status` varchar(20) NOT NULL DEFAULT 'NEW',
+  `md5` varchar(32) DEFAULT NULL COMMENT 'md5',
+  `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `src_user` text COMMENT 'source user',
+  `src_ip` varchar(20) DEFAULT NULL COMMENT 'source ip',
+  `app_name` varchar(128) DEFAULT NULL,
+  `tenant_id` varchar(128) DEFAULT '' COMMENT '租户字段',
+  `c_desc` varchar(256) DEFAULT NULL,
+  `c_use` varchar(64) DEFAULT NULL,
+  `effect` varchar(64) DEFAULT NULL,
+  `type` varchar(64) DEFAULT NULL,
+  `c_schema` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='config_info_item';
+
 /******************************************/
 /*   数据库全名 = nacos_config   */
 /*   表名称 = config_info_aggr   */
